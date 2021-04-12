4422a189821dc66d2acbc28927d2700b377e11ca
santi <72443324+santikarate@users.noreply.github.com>
2021-04-12 16:29:51 +0200

    Sala de enemics creada, amb una oleada
    un cop a dintre la sala no podras sortir fins que no matis els enemics

---
+++ Assets/Scripts/Detectar_Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+public class Detectar_Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject col;
+    private bool accio;
+    private void Start()
+    {
+        accio = false;
+    }
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        yield return new WaitForSeconds(0f);
+        if (collision.tag == "Player")
+        {
+            print("ey");
+            if (!accio)
+            {
+                accio = true;
+                print("hola");
+                col.GetComponent<comprovarEnemic>().deteccioDePlayer();
+            }
+        }
+    }
+}
--- Assets/Scripts/comprovarEnemic.cs
+++ Assets/Scripts/comprovarEnemic.cs
@@ -6,35 +6,78 @@ using UnityEngine;
 public class comprovarEnemic : MonoBehaviour
 {
     public List<GameObject> enemic;
+    public List<GameObject> enemic2;
     public BoxCollider2D porta;
-    private int capacitat;
+    public BoxCollider2D portaAnterior;
+    public BoxCollider2D entrada;
+    public BoxCollider2D porta2;
+    private int capacitat, capacitat2;
     private int total;
+    bool secondRound;
     // Start is called before the first frame update
     void Start()
     {
         porta.enabled = true;
+        porta2.enabled = true;
+        portaAnterior.enabled = false;
         capacitat = enemic.Count;
+        secondRound = false;
+        capacitat2 = enemic2.Count;
     }
 
     // Update is called once per frame
     void Update()
     {
-        total = 0;
-        if (porta.enabled)
+        if (!secondRound)
         {
-            for (int i = 0; enemic.Count > i; i++)
+            total = 0;
+            if (porta.enabled)
             {
-                if (enemic[i] == null)
+                for (int i = 0; enemic.Count > i; i++)
                 {
-                    total++;
+                    if (enemic[i] == null)
+                    {
+                        total++;
+                    }
                 }
             }
-        }
-        if (total == capacitat)
+            if (total == capacitat)
+            {
+                segonaRonda();
+            }
+        } else
         {
-            porta.enabled = false;
+            total = 0;
+            if (porta.enabled)
+            {
+                for (int i = 0; enemic2.Count > i; i++)
+                {
+                    if (enemic2[i] == null)
+                    {
+                        total++;
+                    }
+                }
+            }
+            if (total == capacitat2)
+            {
+                porta.enabled = false;
+                porta2.enabled = false;
+                portaAnterior.enabled = false;
+            }
         }
     }
-        
+
+    void segonaRonda()
+    {
+        for (int i = 0; enemic2.Count > i; i++)
+        {
+            enemic2[i].SetActive(true);
+        }
+        secondRound = true;
+    }
+    public void deteccioDePlayer()
+    {
+        portaAnterior.enabled = true;
+    }
     
 }
