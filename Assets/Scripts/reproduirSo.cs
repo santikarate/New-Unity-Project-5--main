d46aeb8dea90c07fce74871d24fb1ffbad4ad9d3
santi <72443324+santikarate@users.noreply.github.com>
2021-04-30 17:07:20 +0200

    So de UI fet

---
+++ Assets/Scripts/reproduirSo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reproduirSo : MonoBehaviour
+{
+    public AudioSource so;
+    public void reproduir() {
+        so.Play();
+    }
+}
